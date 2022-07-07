759c7c6ca6a3aad5b26e9131ba732e6d7aba6005
Nigel Tan <ntan1@tellico-compute1.local>
2022-07-07 16:31:15 -0400

    Add utils.hpp to hold general DEBUG_PRINT macros and other widely used functions.

---
+++ include/utils.hpp
@@ -0,0 +1,12 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+//#define DEBUG
+
+#ifdef DEBUG
+#define DEBUG_PRINT(...) do{ fprintf( stderr, __VA_ARGS__ ); } while( false )
+#else
+#define DEBUG_PRINT(...) do{ } while ( false )
+#endif
+
+#endif
